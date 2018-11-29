1f72ecc2b903ed5e0264cf2dbb42c7048c82e338
Michael Fleshman <michaelfleshman90@gmail..com>
2018-11-28 22:53:52 -0500

    clicky game

--- clickygame/src/components/clickygamecontainer.js
+++ clickygame/src/components/clickygamecontainer.js
@@ -1,19 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
+//import Container from './Container';
 
-class Clickygame extends Component {
+
+class Clickygame extends React.Component {
 
     state = {
-        count: 0
-    };
+      count: 0
+  };
 
-    imageClick = () => {
+  imageClick = () => {
 
-        this.setState({ count: this.state.count + 1});
-    };
+      this.setState({ count: this.state.count + 1});
+  };
 
-    render() {
-        return (
+    // render() {
+    //   return (
+    //     <div className="App">
+    //       <header className="App-header">
+    //         <h1>Clicky Game!</h1>
             
-        )
-    }
+    //       </header>
+    //     </div>
+    //   );
+    // }
 }
+
+export default Clickygame;
